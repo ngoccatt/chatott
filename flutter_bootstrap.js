e845290c5c2d66764c51d09487887c04543dc922
ngoccatt <ngoccatt@users.noreply.github.com>
2024-05-03 18:11:26 +0000

    deploy: fb82bec96d3edd5ce9ecebe653b2e20d2ec408e6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b4bfd459865a8d636f26aca0d330ae297c006c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1269567380"
+    serviceWorkerVersion: "1505226572"
   }
 });
